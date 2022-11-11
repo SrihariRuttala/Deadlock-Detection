983b2def1e310c8badc8d3388ba95088254541dc
Srihari Ruttala <999srihari@gmail.com>
2022-11-11 22:51:10 +0530

    Create Graph Done

--- deadlock.py
+++ deadlock.py
@@ -1,9 +1,7 @@
+import numpy as np
+# file = input("Enter the file path : ")
 
-
-
-file = input("Enter the file path : ")
-
-with open(file, 'r') as f:
+with open('input2.txt', 'r') as f:
 	data = f.readlines()
 	processes = []
 	need_release = []
@@ -15,13 +13,41 @@ with open(file, 'r') as f:
 		resources.append(int(data[i][4]))
 
 if max(processes) < max(resources):
-	n = max(resources)
+	n = max(resources)+1
 else:
-	n = max(processes)
+	n = max(processes)+1
 
 m = len(data)
 
-matrix = [[0]*n] * n
+# matrix = [[0]*n] * n
+matrix = np.full((n,n),0)
+
+priority_map = {}
+
+resource_map = {}
+for i in resources:
+	resource_map[i] = 0
 
 for i in range(m):
-	if need_release == 'N' and m[]
+
+	if need_release[i] == 'N' and resource_map[resources[i]] == 0:
+		# print(proce)
+		matrix[processes[i]][resources[i]] = 1
+		resource_map[i] = 1
+	elif need_release[i] == 'N' and resource_map[resources[i]] == 1:
+		matrix[resources[i]][processes[i]] = 1
+		if resources[i] not in priority_map:
+			priority_map[resources[i]] = [processes[i]]
+		else:
+			# val = list(priority_map[resources[i]][-1].values())[0] + 1
+			priority_map[resources[i]].append(processes[i])
+	elif need_release[i] == 'R':
+		matrix[processes[i]][resources[i]] = 0
+		# print(matrix)
+		resource_map[i] = 0
+		if resources[i] in priority_map.keys():
+			proc = priority_map[resources[i]].pop(0)
+			matrix[resources[i]][proc] = 0
+			matrix[proc][resources[i]] = 1
+
+print(matrix)
